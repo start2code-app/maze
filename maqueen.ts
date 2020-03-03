a0dcbb6f39fe01ece52f48cacd2999e32a59e8fc
start2code-app <53997286+start2code-app@users.noreply.github.com>
2020-03-03 15:59:17 +0000

    Update maqueen.ts

--- maqueen.ts
+++ maqueen.ts
@@ -15,7 +15,7 @@ MicroSeconds
 
 
 //% weight=10 color=#008B00 icon="\uf136" block="maqueen"
-namespace maqueen{
+namespace maqueen-maze{
   
     export class Packeta {
         public mye: string;
